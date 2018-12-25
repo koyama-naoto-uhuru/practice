44fbd7dbb476d2456f4a2e08ae5f486eadd63fea
d.kanai <d.kanai@pastoraldog.com>
2018-12-25 14:47:51 +0900

    refactor test

--- java_junit/src/test/java/vendingmachine/VendingMachineTest.java
+++ java_junit/src/test/java/vendingmachine/VendingMachineTest.java
@@ -110,138 +110,142 @@ public class VendingMachineTest {
         }
     }
 
-    @Test
-    void chargeValidMoney() {
-        Arrays.asList(10, 50, 500, 1000).forEach(charge -> {
-            assertThat(new VendingMachine().charge(charge).currentCharge(), is(charge));
-        });
-    }
-
-    @Test
-    void chargeInvalidMoney() {
-        Arrays.asList(1, 5, 5000, 10000).forEach(charge -> {
-            assertThat(new VendingMachine().charge(charge).currentCharge(), is(0));
-        });
-    }
-
-    @Test
-    void multiCharge() {
-        Integer charge = new VendingMachine().charge(100).charge(1000).currentCharge();
-        assertThat(charge, is(1100));
-    }
-
-    @Test
-    void resetCharge() {
-        VendingMachine vm = new VendingMachine().charge(100).charge(1000);
-        assertThat(vm.resetCharge(), is(1100));
-        assertThat(vm.currentCharge(), is(0));
-    }
-
-    @Test
-    void has5CokeInit() {
-        VendingMachine vm = new VendingMachine();
-        assertThat(vm.inventory.drinks.size(), is(5));
-        vm.inventory.drinks.forEach((drink) -> {
-            assertThat(drink.name, is("coke"));
-            assertThat(drink.price, is(120));
-        });
-
-    }
-
-    @Test
-    void addDrink() {
-        VendingMachine vm = new VendingMachine().addDrink("water", "100");
-        assertThat(vm.inventory.drinks.size(), is(6));
-        assertThat(vm.inventory.drinks.get(5).name, is("water"));
-        assertThat(vm.inventory.drinks.get(5).price, is(100));
-    }
-
-    @Test
-    void cantAddOverMaxDrink() {
-        VendingMachine vm = new VendingMachine();
-        for (int i = 1; i <= 11; i++) {
-            vm.addDrink("water", "100");
-        }
-        assertThat(vm.inventory.countBy("water"), is(10));
-    }
-
-
-    @Test
-    void inventory() {
-        VendingMachine vendingMachine = new VendingMachine();
-        assertThat(vendingMachine.inventory(), is("coke 120yen: 5"));
-        vendingMachine.addDrink("coke", "120");
-        assertThat(vendingMachine.inventory(), is("coke 120yen: 6"));
-        vendingMachine.addDrink("water", "100");
-        assertThat(vendingMachine.inventory(), is("coke 120yen: 6\nwater 100yen: 1"));
-    }
-
-
     @Nested
-    public class canBuy {
-        private VendingMachine vendingMachine;
-
-        @BeforeEach
-        void before() {
-            vendingMachine = new VendingMachine();
-        }
+    class BottomUP {
 
         @Test
-        void lessChargeEnoughInventory() {
-            vendingMachine.addDrink("tea", "150");
-            vendingMachine.charge(100).charge(10);
-            assertThat(vendingMachine.canBy("tea"), is(false));
-            assertThat(vendingMachine.canBy("coke"), is(false));
-        }
-
-        @Test
-        void enoughChargeAndInventory() {
-            vendingMachine.charge(100).charge(10).charge(10);
-            assertThat(vendingMachine.canBy("coke"), is(true));
-        }
-
-        @Test
-        void lessInventoryEnoughtCharge() {
-            vendingMachine.charge(100).charge(50);
-            assertThat(vendingMachine.canBy("tea"), is(false));
-        }
-    }
-
-    @Nested
-    public class buy {
-
-        private VendingMachine vm;
-
-        @BeforeEach
-        void before() {
-            vm = new VendingMachine();
-            vm.addDrink("tea", "150");
-            vm.charge(500);
-        }
-
-        @Test
-        void updateCharge() {
-            Arrays.asList(
-                    new String[]{"coke", "380"},
-                    new String[]{"tea", "230"}
-            ).forEach(pattern -> {
-                vm.buy(pattern[0]);
-                assertThat(vm.currentCharge(), is(Integer.valueOf(pattern[1])));
+        void chargeValidMoney() {
+            Arrays.asList(10, 50, 500, 1000).forEach(charge -> {
+                assertThat(new VendingMachine().charge(charge).currentCharge(), is(charge));
             });
         }
 
         @Test
-        void updateInventory() {
-            vm.buy("tea");
-            assertThat(vm.inventory(), is("coke 120yen: 5"));
+        void chargeInvalidMoney() {
+            Arrays.asList(1, 5, 5000, 10000).forEach(charge -> {
+                assertThat(new VendingMachine().charge(charge).currentCharge(), is(0));
+            });
         }
 
         @Test
-        void cantBuyWhenNoInventory() {
-            vm.buy("tea");
-            vm.buy("tea");
-            assertThat(vm.currentCharge(), is(350));
-            assertThat(vm.inventory(), is("coke 120yen: 5"));
+        void multiCharge() {
+            Integer charge = new VendingMachine().charge(100).charge(1000).currentCharge();
+            assertThat(charge, is(1100));
+        }
+
+        @Test
+        void resetCharge() {
+            VendingMachine vm = new VendingMachine().charge(100).charge(1000);
+            assertThat(vm.resetCharge(), is(1100));
+            assertThat(vm.currentCharge(), is(0));
+        }
+
+        @Test
+        void has5CokeInit() {
+            VendingMachine vm = new VendingMachine();
+            assertThat(vm.inventory.drinks.size(), is(5));
+            vm.inventory.drinks.forEach((drink) -> {
+                assertThat(drink.name, is("coke"));
+                assertThat(drink.price, is(120));
+            });
+
+        }
+
+        @Test
+        void addDrink() {
+            VendingMachine vm = new VendingMachine().addDrink("water", "100");
+            assertThat(vm.inventory.drinks.size(), is(6));
+            assertThat(vm.inventory.drinks.get(5).name, is("water"));
+            assertThat(vm.inventory.drinks.get(5).price, is(100));
+        }
+
+        @Test
+        void cantAddOverMaxDrink() {
+            VendingMachine vm = new VendingMachine();
+            for (int i = 1; i <= 11; i++) {
+                vm.addDrink("water", "100");
+            }
+            assertThat(vm.inventory.countBy("water"), is(10));
+        }
+
+
+        @Test
+        void inventory() {
+            VendingMachine vendingMachine = new VendingMachine();
+            assertThat(vendingMachine.inventory(), is("coke 120yen: 5"));
+            vendingMachine.addDrink("coke", "120");
+            assertThat(vendingMachine.inventory(), is("coke 120yen: 6"));
+            vendingMachine.addDrink("water", "100");
+            assertThat(vendingMachine.inventory(), is("coke 120yen: 6\nwater 100yen: 1"));
+        }
+
+
+        @Nested
+        public class canBuy {
+            private VendingMachine vendingMachine;
+
+            @BeforeEach
+            void before() {
+                vendingMachine = new VendingMachine();
+            }
+
+            @Test
+            void lessChargeEnoughInventory() {
+                vendingMachine.addDrink("tea", "150");
+                vendingMachine.charge(100).charge(10);
+                assertThat(vendingMachine.canBy("tea"), is(false));
+                assertThat(vendingMachine.canBy("coke"), is(false));
+            }
+
+            @Test
+            void enoughChargeAndInventory() {
+                vendingMachine.charge(100).charge(10).charge(10);
+                assertThat(vendingMachine.canBy("coke"), is(true));
+            }
+
+            @Test
+            void lessInventoryEnoughtCharge() {
+                vendingMachine.charge(100).charge(50);
+                assertThat(vendingMachine.canBy("tea"), is(false));
+            }
+        }
+
+        @Nested
+        public class buy {
+
+            private VendingMachine vm;
+
+            @BeforeEach
+            void before() {
+                vm = new VendingMachine();
+                vm.addDrink("tea", "150");
+                vm.charge(500);
+            }
+
+            @Test
+            void updateCharge() {
+                Arrays.asList(
+                        new String[]{"coke", "380"},
+                        new String[]{"tea", "230"}
+                ).forEach(pattern -> {
+                    vm.buy(pattern[0]);
+                    assertThat(vm.currentCharge(), is(Integer.valueOf(pattern[1])));
+                });
+            }
+
+            @Test
+            void updateInventory() {
+                vm.buy("tea");
+                assertThat(vm.inventory(), is("coke 120yen: 5"));
+            }
+
+            @Test
+            void cantBuyWhenNoInventory() {
+                vm.buy("tea");
+                vm.buy("tea");
+                assertThat(vm.currentCharge(), is(350));
+                assertThat(vm.inventory(), is("coke 120yen: 5"));
+            }
         }
     }
 }
