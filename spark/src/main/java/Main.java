ea7ae605b45079a0a61624b6f51cd3e94ed05a7e
d.kanai <d.kanai@pastoraldog.com>
2019-02-18 13:09:05 +0900

    add index action

--- spark/src/main/java/Main.java
+++ spark/src/main/java/Main.java
@@ -1,7 +1,17 @@
+import spark.Request;
+import spark.Response;
+import spark.Route;
+
 import static spark.Spark.*;
 
 public class Main {
     public static void main(String[] args) {
-        get("/hello", (req, res) -> "Hello World");
+        get("/", IndexController.index);
+    }
+
+    private static class IndexController {
+        public static Route index = (Request request, Response response) -> {
+            return "hello";
+        };
     }
 }
